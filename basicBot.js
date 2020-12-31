76e65e18cb534d7f7d429a49230b532217fb8d1e
- <76801064+kirnz@users.noreply.github.com>
2020-12-31 17:06:26 +1300

    update

--- basicBot.js
+++ basicBot.js
@@ -251,7 +251,7 @@
         loggedInID: null,
         scriptLink: 'https://raw.githubusercontent.com/kirnz/source/master/basicBot.js',
         cmdLink: 'http://git.io/245Ppg',
-        chatLink: 'https://raw.githack.com/basicBot/source/master/lang/en.json',
+        chatLink: 'https://raw.githack.com/kirnz/source/master/lang/en.json',
         chat: null,
         loadChat: loadChat,
         retrieveSettings: retrieveSettings,
@@ -259,8 +259,8 @@
         settings: {
             botName: 'basicBot Mozzle Edition',
             language: 'english',
-            chatLink: 'https://raw.githack.com/basicBot/source/master/lang/en.json',
-            scriptLink: 'https://raw.githubusercontent.com/kirnz/source/master/basicBot.js',
+            chatLink: 'https://raw.githack.com/kirnz/source/master/lang/en.json',
+            scriptLink: 'https://raw.githack.com/kirnz/source/master/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
@@ -392,7 +392,8 @@
                     var ind = Math.floor(Math.random() * basicBot.room.roulette.participants.length);
                     var winner = basicBot.room.roulette.participants[ind];
                     basicBot.room.roulette.participants = [];
-                    var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
+                    var pos = 1;
+                    //var pos = Math.floor((Math.random() * API.getWaitList().length) + 1);
                     var user = basicBot.userUtilities.lookupUser(winner);
                     var name = user.username;
                     API.sendChat(subChat(basicBot.chat.winnerpicked, {
