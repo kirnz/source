925f83feb8e07674443ebb1cd8d83cb83974d505
- <76801064+kirnz@users.noreply.github.com>
2021-01-01 14:21:48 +1300

    all the thingz

--- basicBot.js
+++ basicBot.js
@@ -421,14 +421,14 @@
                     //API.sendChat("[Debug] User variable is " + user);
                     var name = user.username;
                     //API.sendChat("[Debug] Name variable is " + name);
-                    API.chatLog("Name is " + name);
+                    //API.chatLog("Name is " + name);
                     
 
                     if(name == null || name == 'undefined') {
-                        API.chatLog("if condition met")
+                        //API.chatLog("if condition met")
                         API.sendChat("Ugh oh, nobody joined the :musical_note:Limitless :diamonds:Roulette:spades:. A winner could not be chosen.");
                     } else {
-                        API.chatLog("else condition met")
+                        //API.chatLog("else condition met")
                         API.sendChat(subChat(basicBot.chat.winnerpicked, {
                             name: name,
                             position: pos
