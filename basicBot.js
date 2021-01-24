f633087f4f7334113c31270dba041b6c52757070
- <76801064+kirnz@users.noreply.github.com>
2021-01-24 17:42:08 +1300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3392,7 +3392,7 @@
 
             rouletteCommand: {
                 command: 'roulette',
-                rank: 'mod',
+                rank: 'manager',
                 type: 'exact',
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
